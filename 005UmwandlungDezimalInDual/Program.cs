8b00269da7c06b787d1fe9dd4c7a0d95674213f8
Jannick Jungbluth <jannick2.0@googlemail.com>
2017-03-30 19:40:32 +0200

    Init

---
+++ 005UmwandlungDezimalInDual/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005UmwandlungDezimalInDual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int dezimal = Convert.ToInt16(Console.ReadLine());
+                String dual = "";
+                while (dezimal > 0)
+                {
+                    dual = (dezimal % 2) + dual;
+                    dezimal = dezimal / 2;
+                }
+                Console.WriteLine("Dualzahl: {0}", dual);
+                Console.WriteLine("Möchten Sie noch eine Zahl umwandeln? (ja/nein)");
+            } while (Console.ReadLine() == "ja");
+        }
+    }
+}
